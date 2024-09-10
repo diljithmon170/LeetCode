0e07a764cb046310dc319f48a6fb0d0e45981291
Diljith D <diljithmon170@gmail.com>
2024-09-10 16:09:30 +0530

    added new index based pgm code

--- merge_sor_arr.py
+++ merge_sor_arr.py
@@ -21,3 +21,18 @@ else:
     merge=n1+n2
     nums1=sorted(merge)
 print(nums1)
+
+
+# idx = m + n - 1
+# while m > 0 and n > 0:
+#     if nums1[m - 1] < nums2[n - 1]:
+#         nums1[idx] = nums2[n - 1]
+#         n -= 1
+#     else:
+#         nums1[idx] = nums1[m - 1]
+#         m -= 1
+#     idx -= 1
+# while n > 0:
+#     nums1[idx] = nums2[n - 1]
+#     n -= 1
+#     idx -= 1
