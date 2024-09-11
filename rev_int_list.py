7088b7d4f6a29eb90ad43b59d071fe3058605133
Diljith D <diljithmon170@gmail.com>
2024-09-12 00:19:53 +0530

    modifications of rev int list with new solution

--- rev_int_list.py
+++ rev_int_list.py
@@ -1,17 +1,29 @@
-x=-123000
-s=str(x)
-rv=s[::-1]
-l=list(rv)
-print(rv)
-print(l)
-i=0
-while i < len(l):
-    if l[i] == '0':
-        l.pop(i)
-        break
-print(l)
-# l=list(s)
-# r=[]
-# r=l.reverse(s)
-# print(l)
-# print(r)
+# my sol
+
+x=1534236469
+# s=str(x)
+# r=""
+# rv=s[::-1]
+# l=list(rv)
+# if len(l)>1:
+#     while l[0]=="0":
+#         l.pop(0)
+# if l[-1]=="-":
+#     l=[l[-1]] + l[:-1]
+# for j in range(len(l)):
+#     r+=l[j]
+# x=int(r)
+# print(x)
+
+
+#solutio !
+
+mod = 2**31  # Modulo for overflow checking
+sign = -1 if x < 0 else 1
+x *= sign
+reversed_number = int(str(x)[::-1])
+# Check for overflow
+if reversed_number > mod - 1:
+    print(0)
+else:
+    print(sign * reversed_number)
