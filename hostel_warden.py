36ee47433f96b67280dbb4d0dc1ae7fbc435b950
Diljith D <diljithmon170@gmail.com>
2024-09-29 16:02:37 +0530

    hostel warden

---
+++ hostel_warden.py
@@ -0,0 +1,32 @@
+n=13
+stud="Kkunjkhahorin"
+st_pr=[]
+st_cp=[]
+st_ac=[]
+for i in stud:
+    # st_ac.append(ord(i))
+    t=ord(i)
+    for i in range(2,(t//2)+1):
+        if t%i==0:
+            flag=False 
+        else:
+            flag=True     
+    if flag==True:
+        st_pr.append(t)
+    else:
+        st_cp.append(t)
+st_pr.sort()
+st_cp.sort(reverse=True)
+print(st_pr)
+print(st_cp)
+for i in st_pr:
+    st_ac.append(i)
+for i in st_cp:
+    st_ac.append(i)
+print(st_ac)
+s=""
+for i in st_ac:
+    s+=chr(i)
+print(s)
+        
+
