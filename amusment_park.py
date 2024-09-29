686e66342c681d03b2572b10bb68a944feab3041
Diljith D <diljithmon170@gmail.com>
2024-09-29 10:45:34 +0530

    amusment_park

--- amusment_park.py
+++ amusment_park.py
@@ -1,11 +1,20 @@
-price="203"
+price="2500"
 turn=2
 s=[]
+t=[]
 s=list(str(price))
+t=list(str(price))
 print(s)
 while turn>0:
-    max_no=max(s)
-    s.remove(max_no)
+    max_no=max(t)
+    t.remove(max_no)
+    z=t.count('0')
+    print(z)
+    if z==len(t):
+        s.remove('0')
+    else:
+        max_no=max(s)
+        s.remove(max_no)
     turn-=1
 print(s)
 
