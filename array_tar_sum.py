b58368039a7483dcf9a4776a70df29c5fd70adab
Diljith D <diljithmon170@gmail.com>
2024-10-02 23:21:47 +0530

    array sum

--- array_tar_sum.py
+++ array_tar_sum.py
@@ -1,21 +1,19 @@
-arr=[1,2,3,4,5]
-tar=10
-flag=False
-i=0
-j=len(arr)-1
-while flag==False:
-    if i<j:
-        if arr[0]==tar:
-            flag=True
-            print(arr[0])
-            break
-        elif tar==arr[i]+arr[j]:
-            flag=True
-            print(arr[i],arr[j])
-        elif tar>arr[i]+arr[j]:
-            i+=1
-        elif tar<arr[i]+arr[j]:
-            j-=1
+def sum(arr,tar):
+    l=r=0,len(arr)-1
+    while l<r:
+        temp_sum=arr[l]+arr[r]
+        if temp_sum==tar:
+            return (arr[l],arr[r])
+        elif temp_sum<tar:
+            l+=1
         else:
-            print(-1)
-            flag=True
+            r+=1
+    return -1
+
+arr=[1,4,8]
+tar=9
+result=sum(arr,tar)
+if result:
+    print(result)
+else:
+    print("not found")
