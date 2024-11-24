01c58884302a6a0e35e3ebde7a8baa08d18dfc2c
Diljith D <diljithmon170@gmail.com>
2024-11-24 21:40:36 +0530

    ffyf

--- hi.py
+++ hi.py
@@ -8,22 +8,5 @@ print("hi")
 print("hi")
 print("hi")
 print("hi")
-print("hi")
-print("hi")
-print("hi")
-print("hi")
-print("hi")
-print("hi")
-print("hi")
-
-print("hi")
-print("hi")
-print("hi")
-print("hi")
-
-print("hi")
-print("hi")
-print("hi")
-print("hi")
 
 
